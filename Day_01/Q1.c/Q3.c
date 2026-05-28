4468ea6db7cc317b0fd05258207bd110a8f6236b
Tripti <triptidhariwal@gmail.com>
2026-05-28 18:51:58 +0530

    Added question 3 of Day 1

---
+++ Day_01/Q1.c/Q3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i,fact=1,n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i){
+        fact*=i;
+    }
+    printf("Factorial of %d is %d",n,fact);
+    return 0;
+}
