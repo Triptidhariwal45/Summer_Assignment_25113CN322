1a676db3455cdaa0108dab1f1dc94eb55e2299f7
Tripti <triptidhariwal@gmail.com>
2026-06-02 12:13:54 +0530

    Added question 5 of Day 2

---
+++ Day_02/Q5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int mian(){
+    int num ,sum=0,digit;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    while(num!=0){
+        digit=num%10;
+        sum+=digit;
+        num/=10;
+    }
+    printf("The sum of the digits is:%d",sum);
+    return 0;
+}
