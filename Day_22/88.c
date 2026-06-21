fe82ed6b8683ae1bad6b90c30c1bd6c43a6a5f9f
Tripti <triptidhariwal@gmail.com>
2026-06-21 20:23:06 +0530

    Added Question 88 of Day 22

---
+++ Day_22/88.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], newStr[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ' && str[i] != '\n') {
+            newStr[j++] = str[i];
+        }
+    }
+
+    newStr[j] = '\0';
+
+    printf("String after removing spaces: %s\n", newStr);
+
+    return 0;
+}
