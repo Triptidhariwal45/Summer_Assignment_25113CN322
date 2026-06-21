bde67ebada0b2831737db0be86e0c7cd78e13c46
Tripti <triptidhariwal@gmail.com>
2026-06-21 16:34:42 +0530

    Added Question 63 of Day 16

---
+++ Day_16/63.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+
+    int found = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == sum) {
+                printf("Pair found: (%d, %d)\n", arr[i], arr[j]);
+                found = 1;
+            }
+        }
+    }
+
+    if (!found) {
+        printf("No pair found.\n");
+    }
+
+    return 0;
+}
